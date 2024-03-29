605504a9ed30bbf2e8ba0f551dad603f87bd30dd
CODEKARTEL <codekartel@localhost.localdomain>
2024-03-29 05:30:23 +0300

    solution code

--- pint.c
+++ pint.c
@@ -1,21 +1,19 @@
 #include "monty.h"
-
 /**
- * print_top_of_stack - Prints the top element of the stack
- * @head: Pointer to the head of the stack
- * @line_number: Line number in the file
- * Return: No return value
- */
-void print_top_of_stack(stack_t **head, unsigned int line_number)
+ * f_pint - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pint(stack_t **head, unsigned int counter)
 {
-    if (*head == NULL)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", (*head)->n);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
