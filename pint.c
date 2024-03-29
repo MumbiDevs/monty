9571bc6d5f3841e0f8e7645f5b1ad758d74fd6b6
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-03-29 05:06:18 +0300

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,100 +1,21 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define STACK_SIZE 100
 
 /**
- * stack - Stack data structure
- * @stack: Pointer to the top of the stack
- * @top: Index of the top element in the stack
+ * print_top_of_stack - Prints the top element of the stack
+ * @head: Pointer to the head of the stack
+ * @line_number: Line number in the file
+ * Return: No return value
  */
-typedef struct stack {
-    int stack[STACK_SIZE];
-    int top;
-} Stack;
-
-/**
- * push - Pushes an element onto the stack
- * @s: Pointer to the stack
- * @value: Value to be pushed onto the stack
- */
-void push(Stack *s, int value)
+void print_top_of_stack(stack_t **head, unsigned int line_number)
 {
-    if (s->top == STACK_SIZE - 1)
+    if (*head == NULL)
     {
-        fprintf(stderr, "Stack overflow\n");
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
         exit(EXIT_FAILURE);
     }
-    s->stack[++(s->top)] = value;
-}
 
-/**
- * pint - Prints the value at the top of the stack
- * @s: Pointer to the stack
- */
-void pint(Stack *s)
-{
-    if (s->top == -1)
-    {
-        fprintf(stderr, "L<line_number>: can't pint, stack empty\n");
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", s->stack[s->top]);
-}
-
-/**
- * main - Entry point of the Monty interpreter
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Cannot open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    Stack stack;
-    stack.top = -1;
-
-    char line[100];
-    while (fgets(line, sizeof(line), file))
-    {
-        char *token = strtok(line, " \t\n");
-        if (token)
-        {
-            if (strcmp(token, "push") == 0)
-            {
-                token = strtok(NULL, " \t\n");
-                if (token)
-                {
-                    int value = atoi(token);
-                    push(&stack, value);
-                }
-                else
-                {
-                    fprintf(stderr, "Error: push requires an argument\n");
-                    return EXIT_FAILURE;
-                }
-            }
-            else if (strcmp(token, "pint") == 0)
-            {
-                pint(&stack);
-            }
-        }
-    }
-
-    fclose(file);
-    return EXIT_SUCCESS;
+    printf("%d\n", (*head)->n);
 }
